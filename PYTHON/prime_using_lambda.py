c54633e4dd2ba6fd23f64f3fdcd847fc32c8c4e4
vaibhavkundu123 <vaibhavkundu69@gmail.com>
2023-09-15 15:54:44 +0530

    updated PYTHON

--- PYTHON/prime_using_lambda.py
+++ PYTHON/prime_using_lambda.py
@@ -1,11 +1,3 @@
-is_prime = lambda x: all(x % i != 0 for i in range(2, int(x**0.5) + 1)) and x > 1
+start_range, end_range= (int(input("Enter the start of the range: ")), int(input("Enter the end of the range: ")))
 
-
-start_range = int(input("Enter the start of the range: "))
-end_range = int(input("Enter the end of the range: "))
-
-
-prime_numbers = [num for num in range(start_range, end_range + 1) if is_prime(num)]
-
-
-print(f"Prime numbers between {start_range} and {end_range}: {prime_numbers}")
+print(f"Prime numbers between {start_range} and {end_range}: ", list(filter(lambda x: all(x % i != 0 for i in range(2, int(x**0.5) + 1)) and x > 1, range(start_range, end_range + 1))))
