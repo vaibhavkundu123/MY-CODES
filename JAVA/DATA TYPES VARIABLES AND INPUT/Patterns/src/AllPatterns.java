bcff8aaa25aa97b864d2dc7fe858aa3d359fffdf
VAIBHAV KUNDU <vaibhavkundu69@gmail.com>
2024-02-25 15:30:35 +0530

    Files added

---
+++ JAVA/DATA TYPES VARIABLES AND INPUT/Patterns/src/AllPatterns.java
@@ -0,0 +1,7 @@
+public class AllPatterns {
+    public static void main(String[] args) {
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+        System.out.println("\n\n* * * * *\n* * * *\n* * *\n* *\n*");
+        System.out.println("\n\n        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+    }
+}
