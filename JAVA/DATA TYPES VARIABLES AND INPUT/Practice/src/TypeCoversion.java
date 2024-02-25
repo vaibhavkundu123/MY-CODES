3b0b495dd6fbb8e23b19bd2dc5077c587a4e260b
VAIBHAV KUNDU <vaibhavkundu69@gmail.com>
2024-02-25 19:45:28 +0530

    files modified

---
+++ JAVA/DATA TYPES VARIABLES AND INPUT/Practice/src/TypeCoversion.java
@@ -0,0 +1,9 @@
+public class TypeCoversion {
+    public static void main(String[] args) {
+        float myFloat = 5;
+        System.out.println(myFloat);
+
+        int myInt = (int) 5.45f;
+        System.out.println(myInt);
+    }
+}
