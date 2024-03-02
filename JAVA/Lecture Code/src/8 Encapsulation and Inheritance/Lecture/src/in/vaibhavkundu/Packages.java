091bf2b27738743214bda42b7673148c2a67e39b
VAIBHAV KUNDU <vaibhavkundu69@gmail.com>
2024-03-02 20:35:26 +0530

    files added

---
+++ JAVA/Lecture Code/src/8 Encapsulation and Inheritance/Lecture/src/in/vaibhavkundu/Packages.java
@@ -0,0 +1,10 @@
+package in.vaibhavkundu;
+
+import java.util.Scanner;
+
+public class Packages {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        in.vaibhav.Packages packages = new in.vaibhav.Packages();
+    }
+}
